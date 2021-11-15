671493f441332925658025d3f4d406cdce4270d8
Cesar Alcancio <cesar.alcancio@gmail.com>
2021-11-15 14:59:08 -0300

    Add first version of lista transferencias widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.00'),
-                subtitle: Text('200.00'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('300.00'),
-                subtitle: Text('400.00'),
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencia(),
         appBar: AppBar(
           title: Text('Transferencias'),
         ),
@@ -27,3 +10,34 @@ void main() => runApp(MaterialApp(
             FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
       ),
     ));
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.00'),
+            subtitle: Text('200.00'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('300.00'),
+            subtitle: Text('400.00'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('500.00'),
+            subtitle: Text('600.00'),
+          ),
+        ),
+      ],
+    );
+  }
+}
