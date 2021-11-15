df3c0b44cdbe523bdd15ffba0e3fdfa4cc17ad11
Cesar Alcancio <cesar.alcancio@gmail.com>
2021-11-15 15:28:38 -0300

    Create and isolate the formulario

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: ListaTransferencia(),
-        appBar: AppBar(
-          title: Text('Transferencias'),
-        ),
-        floatingActionButton:
-            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+        body: FormularioTransferencia(),
       ),
     ));
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
 class ListaTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ItemTransferencia(Transferencia(10.0, 1234)),
-        ItemTransferencia(Transferencia(20.0, 2345)),
-        ItemTransferencia(Transferencia(30.0, 3456)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferencias'),
+      ),
+      body: Column(
+        children: [
+          ItemTransferencia(Transferencia(10.0, 1234)),
+          ItemTransferencia(Transferencia(20.0, 2345)),
+          ItemTransferencia(Transferencia(30.0, 3456)),
+        ],
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
     );
   }
 }
